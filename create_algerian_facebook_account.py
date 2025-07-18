0b23596c89e9eb58aaa42ec98012d99e126861d4
Marinakhan <marinakhan384@gmail.com>
2025-07-18 14:46:03 +0500

    Update create_algerian_facebook_account.py

--- create_algerian_facebook_account.py
+++ create_algerian_facebook_account.py
@@ -110,7 +110,7 @@ connect_button.click()
 time.sleep(20)
 
 # Navigate to the website
-driver.get("https://www.fakemailgenerator.com/")
+driver.get("https://www.google.com/url?sa=t&source=web&rct=j&opi=89978449&url=https://www.microsoft.com/en-us/microsoft-365/outlook/email-and-calendar-software-microsoft-outlook&ved=2ahUKEwjUo_-njsaOAxXQ0wIHHS0-AZcQFnoECAsQAQ&usg=AOvVaw2FL44I7Ba2xO_S-4IkbBijhtt4IkbBijhttpspsS-4IkbBijhtt4IkbBij")
 
 driver.execute_script("window.stop();")
 
